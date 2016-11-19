ba1b05e8dc1dd134a6190c988b34b32414a4e857
Rory Tuano <tuanorory@gmail.com>
2016-11-20 00:02:24 +0800

    Update greeting

--- app.js
+++ app.js
@@ -56,19 +56,12 @@ if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
   process.exit(1);
 }
 
-request.post({
-    method: 'POST',
-    uri: "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$"+ PAGE_ACCESS_TOKEN,
-    qs: {
-        "setting_type": 'greeting',
-        "greeting":{
-        "text":"Hi {{user_first_name}}, welcome to this bot."
-    }
-    json: true
-}, (err, res, body) => {
-    // Deal with the response
-});
-
+curl -X POST -H "Content-Type: application/json" -d '{
+  "setting_type":"greeting",
+  "greeting":{
+    "text":"Hi {{user_first_name}}, welcome to this bot."
+  }
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token="+ PAGE_ACCESS_TOKEN   
 /*
  * Use your own validation token. Check that the token used in the Webhook 
  * setup is the same token used here.
